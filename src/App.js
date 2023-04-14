883a75d25ae8f853fa5adc0eec4e9d7615761516
vonrashey <vonrashey@gmail.com>
2023-04-14 10:34:41 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'
+
+import { Blog, Header, Possibility, Footer, Features, WhatGPT3 } from './containers'
+import { Article, CTA, Brand, Feature, Navbar } from './componets';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="gradient__bg">
+        <Navbar/>
+        <Header/>
+      </div>
+<Brand/>
+<WhatGPT3/>
+<Features/>
+<Possibility/>
+<CTA/>
+<Blog/>
+<Footer/>
+  </div>
+  )
 }
 
-export default App;
+export default App
